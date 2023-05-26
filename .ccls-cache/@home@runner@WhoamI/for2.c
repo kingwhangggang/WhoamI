d6013a44772925484d09448a662d8bff6893a03d
kingwhangggang <129143192+kingwhangggang@users.noreply.github.com>
2023-05-26 02:16:46 +0000

    while1.c -> for1.c

--- .ccls-cache/@home@runner@WhoamI/for2.c
+++ .ccls-cache/@home@runner@WhoamI/for2.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-main(){
-  int i,j;
+// #include<stdio.h>
+// main(){
+//   int i,j;
   
-  for(j=1; j<10; j++){
-    for(i=1; i<10; i++){
-       printf("%d*%d=%d\n",j,i,i*j);
-    }
-  }
-}
+//   for(j=1; j<10; j++){
+//     for(i=1; i<10; i++){
+//        printf("%d*%d=%d\n",j,i,i*j);
+//     }
+//   }
+// }
